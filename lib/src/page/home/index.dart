0670d0d0a6e3374b007fb71670834849d014981e
sanburnn <dimas00758@gmail.com>
2022-06-14 00:09:33 +0700

    new home

--- lib/src/page/home/index.dart
+++ lib/src/page/home/index.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:user_futsal/src/controller/cekuser_controller.dart';
+import 'package:user_futsal/src/services/assets.dart';
 import 'package:user_futsal/src/services/themes.dart';
 
 class FutsalHomePage extends StatefulWidget {
@@ -23,17 +24,49 @@ class _FutsalHomePageState extends State<FutsalHomePage> {
             SliverToBoxAdapter(
               child: Container(
                 padding: EdgeInsets.only(left: 30, top: 50),
-                child: Text('Hello',
-                    style: GoogleFonts.poppins(
-                        textStyle: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: primaryColorDark,
-                            fontSize: 26))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      logoFutsal,
+                      height: 75,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Obx(() {
+                      if (cek.isLoading.value) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else {
+                        return Text('Hello ${cek.cek.value.username}',
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: primaryColorDark,
+                                    fontSize: 26)));
+                      }
+                    }),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text('Pilih Lapangan',
+                        style: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.w600),
+                        ))
+                  ],
+                ),
               ),
             )
           ];
         },
-        body: Container(),
+        body: Container(
+          height: MediaQuery.of(context).size.height * 0.8,
+          padding: EdgeInsets.only(bottom: 0),
+          child: Container(),
+        ),
       ),
     );
   }
