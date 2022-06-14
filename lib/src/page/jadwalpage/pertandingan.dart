af9344e3122758efbb4d2b1cec410859f6fcc71f
sanburnn <dimas00758@gmail.com>
2022-06-14 22:47:56 +0700

    per

--- lib/src/page/jadwalpage/pertandingan.dart
+++ lib/src/page/jadwalpage/pertandingan.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:user_futsal/src/services/themes.dart';
 
 class PertandinganPage extends StatefulWidget {
   PertandinganPage({Key? key}) : super(key: key);
@@ -11,6 +13,13 @@ class PertandinganPageState extends State<PertandinganPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: primaryColorDark),
+        title: Text(
+          'Jadwal Pertandingan',
+          style: GoogleFonts.poppins(),
+        ),
+      ),
       body: Container(),
     );
   }
