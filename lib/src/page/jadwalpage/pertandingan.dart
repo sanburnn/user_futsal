d5d546e1697144ada40fafc54222a6b6b393696e
sanburnn <dimas00758@gmail.com>
2022-06-15 21:45:45 +0700

    pertadnigan

--- lib/src/page/jadwalpage/pertandingan.dart
+++ lib/src/page/jadwalpage/pertandingan.dart
@@ -21,42 +21,82 @@ class PertandinganPageState extends State<PertandinganPage> {
           style: GoogleFonts.poppins(color: Colors.black),
         ),
       ),
-      body: Container(
-        child: ListView.builder(
-            itemCount: 2,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: EdgeInsets.only(left: 15, right: 15),
-                child: Card(
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 5),
-                        child: Text(
-                          'Jadwal Pertandingan',
-                          style: GoogleFonts.poppins(
-                              textStyle: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 15,
-                                  color: primaryColorDark)),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      )
-                    ],
-                  ),
+      body: NestedScrollView(
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (BuildContext context, bool innerBoxScrolled) {
+          return <Widget>[
+            SliverToBoxAdapter(
+                child: Container(
+              padding: EdgeInsets.only(left: 20, top: 20),
+              child: Text(
+                'Jadwal Pertandingan Pada\nLapangan',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: primaryColorDark),
                 ),
-              );
-            }),
+              ),
+            ))
+          ];
+        },
+        body: Container(
+          child: ListView.builder(
+              itemCount: 2,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(left: 15, right: 15),
+                  child: Card(
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Atas Nama',
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 15,
+                            )),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text('Jam',
+                              style: GoogleFonts.poppins(
+                                  textStyle: TextStyle(fontSize: 15))),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Tanggal',
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(fontSize: 15)),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              }),
+        ),
       ),
     );
   }
