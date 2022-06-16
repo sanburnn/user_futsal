ab3bf2905d71cb8818c72de94fc528055386dbe9
sanburnn <dimas00758@gmail.com>
2022-06-16 12:13:10 +0700

    index

--- lib/src/page/transaction/index.dart
+++ lib/src/page/transaction/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class TransactionPage extends StatefulWidget {
   TransactionPage({Key? key}) : super(key: key);
@@ -10,6 +11,39 @@ class TransactionPage extends StatefulWidget {
 class _TransactionPageState extends State<TransactionPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child: Text('Transaksi',
+              style: GoogleFonts.poppins(
+                  color: Colors.black, fontWeight: FontWeight.w500)),
+        ),
+      ),
+      body: Container(
+        child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: EdgeInsets.only(left: 15, right: 15),
+                child: Card(
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(
+                            'Transaksi',
+                          ))
+                    ],
+                  ),
+                ),
+              );
+            }),
+      ),
+    );
   }
 }
