293782ae700c931c443a7b7d22f01759554b3864
sanburnn <dimas00758@gmail.com>
2022-06-27 00:46:45 +0700

    remove unnecesary import

--- lib/src/controller/register_controller.dart
+++ lib/src/controller/register_controller.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:user_futsal/src/api/repository.dart';
-import 'package:user_futsal/src/model/loginmodel/LoginReqModel.dart';
 import 'package:user_futsal/src/router/constant.dart';
 import 'package:user_futsal/src/services/themes.dart';
 
